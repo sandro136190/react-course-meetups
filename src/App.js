513debee800182a02146eb10828bd193e4d2ba81
Sandro Bochorishvili <sa.bochorishvili@gmail.com>
2025-12-23 23:16:47 +0400

    72. Creating 'Wrapper' Components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import MainNavigation from "./components/layout/MainNavigation";
 import AllMeetpusPage from "./pages/AllMeetups";
 import FaworitesPage from "./pages/Faworites";
 import NewMeetupsPage from "./pages/NewMeetups";
 import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetpusPage />} />
         <Route path="/new-meetup" element={<NewMeetupsPage />} />
         <Route path="/favorites" element={<FaworitesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
