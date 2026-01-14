b04fa585e618bdc26dc390210b4ce39af97a8c0c
Sandro Bochorishvili <sa.bochorishvili@gmail.com>
2026-01-14 23:49:11 +0400

    75. Preparing The App For Http Requests & Adding a Backend

--- src/pages/NewMeetups.jsx
+++ src/pages/NewMeetups.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupsPage = () => {
+  function addMeetupHandler(meetupData) {
+    console.log(meetupData);
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
